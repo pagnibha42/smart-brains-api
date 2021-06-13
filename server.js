1657cdd86d7b10c6df05d9e2bb91d167189d5563
Agnibha Pal <pal.agnibha97@gmail.com>
2021-06-13 22:43:55 +0530

    cors imported

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express'
 import bcrypt from 'bcrypt-nodejs'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const database = {
     users: [
