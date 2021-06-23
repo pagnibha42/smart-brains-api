79a13ceec217c352b807ff8a24859e6c8880a0f1
Agnibha Pal <pal.agnibha97@gmail.com>
2021-06-23 22:37:05 +0530

    reduced lines of code

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express, { json, response } from 'express'
 import bcrypt from 'bcrypt-nodejs'
 import cors from 'cors'
 import knex from 'knex';
+import handleRegister from './controllers/register.js'
+import handleSignIn from './controllers/signin.js';
+import handleProfileGet from './controllers/profile.js';
+import handleImage from './controllers/image.js';
 
 const db = knex({
     client: 'pg',
@@ -29,81 +33,19 @@ app.get('/', (req, res) => {
 
 /*SIGNIN ROUTE*/
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash')
-    .from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if(isValid) {
-            return db.select('*')
-                .from('users')
-                .where('email', '=', req.body.email)
-                .then(user => res.json(user[0]))
-                .catch(err => res.status(400).json('unable to get user'))
-        } else
-            res.status(400).json('wrong credentials')
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', handleSignIn(db, bcrypt)) //one way
 
 /*REGISTER ROUTE*/
 
-app.post('/register', (req, res) => {
-    const { email, name, password} = req.body;
-    
-    const hash = bcrypt.hashSync(password);
-    
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            }).then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => {handleRegister(req, res, db, bcrypt)}) //another way
 
 /*PROFILE ROUTE*/
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id })
-        .then(user => {
-            if (user.length)
-                res.json(user[0])
-            else    
-                res.status(404).json('not found');
-        })
-        .catch(err => res.status(404).json('error getting user'))
-})
+app.get('/profile/:id', (req, res) => {handleProfileGet(req, res, db)})
 
 /*IMAGE ROUTE*/
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where({id})
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => {handleImage(req, res, db)});
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
